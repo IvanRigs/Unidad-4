5ffc002884ed6eeb3770e6e8f621a91ea6145eba
IvanRigs <ivanrios0810@gmail.com>
2024-10-15 11:30:21 -0700

    php

--- 15-oct-24/app/AuthController.php
+++ 15-oct-24/app/AuthController.php
@@ -1,16 +1,37 @@
 <?php
 
+session_start();
+
 class  AuthController
 {
 
-    var_dump($_POST);
+var_dump($_POST);
 
-    if(isset($_POST['action']));
+if(isset($_POST['action'])) {
+switch($_POST['action']) {
+    case 'access' :
+                $authController = new AuthController();
 
-    public function login($email=null, $password=null)
-    {
+                $email = $_POST['correo'];
+                $email = $_POST['contrasenia'];
+
+                $authController
+        }
+}
+
+public function login($email=null, $password=null)
+{
+
+
+    $response = curl_exec($curl);
+
+
+
+    if (isset($response->data->nombre)) {
 
     }
+
+}
 
 }
 
